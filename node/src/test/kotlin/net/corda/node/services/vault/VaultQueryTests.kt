05142cd1efbfcb51aefb18b15657800105a6344a
josecoll <jose.coll@r3cev.com>
2017-06-26 16:39:06 +0100

    Fix broken Unit test.

--- node/src/test/kotlin/net/corda/node/services/vault/VaultQueryTests.kt
+++ node/src/test/kotlin/net/corda/node/services/vault/VaultQueryTests.kt
@@ -1296,7 +1296,8 @@ class VaultQueryTests {
     fun `DEPRECATED DealState dealsWith helper method`() {
         database.transaction {
 
-            val parties = listOf(MEGA_CORP)
+            // specify a different participant to the node owner (MEGA_CORP)
+            val parties = listOf(MINI_CORP)
 
             services.fillWithSomeTestLinearStates(2, "TEST")
             services.fillWithSomeTestDeals(listOf("456"), parties)
@@ -1308,7 +1309,7 @@ class VaultQueryTests {
             // DOCEND
             assertThat(results.states).hasSize(1)
 
-            val states = vaultSvc.dealsWith<DummyDealContract.State>(MEGA_CORP)
+            val states = vaultSvc.dealsWith<DummyDealContract.State>(MINI_CORP)
             assertThat(states).hasSize(1)
         }
     }
